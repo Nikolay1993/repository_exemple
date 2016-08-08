3d680ce293b92903ba4201397df4c249230e79a1
Nikolay <yboomzi@mail.ru>
2016-08-08 18:21:10 +0300

    created project

---
+++ src/main/java/jv1607/App.java
@@ -0,0 +1,10 @@
+package jv1607;
+
+/**
+ * Created by JAVA on 08.08.2016.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello java");
+    }
+}
